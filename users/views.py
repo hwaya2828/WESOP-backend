2ab077b45e3bcfff66b65b05ab31f18d8761f8de
Mac <mac@Macui-MacBook-Pro.local>
2021-05-12 19:48:41 +0900

    ADD: 회원가입 구현 - 이메일, 패스워드 유효성 검사 포함 - 회원가입 완료 즉시 로그인 상태 전환 위해 jwt 리턴

--- users/views.py
+++ users/views.py
@@ -19,20 +19,20 @@ class SignupView(View):
             last_name   = data['last_name']
             phone_number = data.get('phone', None)
 
-            if (re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', data['email']) is None):
+            if (re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.com$', data['email']) is None):
                 return JsonResponse({'MESSAGE':'유효하지 않은 Email'}, status=400)
 
             if (User.objects.filter(email=email).exists()):
                 return JsonResponse({'MESSAGE':'이미 존재하는 계정입니다.'}, status=400)
 
-            if (re.match('^[A-Z0-9]{6,}$', data['password']) is None):
-                return JsonResponse({'MESSAGE':'대문자,숫자,6글자 이상'}, status=400)
+            if (re.match('^[^A-Z]{6,10}$', data['password'])) or (re.match('^[^0-9]{6,10}$', data['password'])):
+                return JsonResponse({'MESSAGE':'대문자,숫자,6글자 이상,10글자 이하'}, status=400)
 
             if not first_name: 
-                return JsonResponse({'MESSAGE':'이름을 입력하세요.'}, status=400)
+                return JsonResponse({'MESSAGE':'성을 입력하세요.'}, status=400)
 
             if not last_name:
-                return JsonResponse({'MESSAGE':'성을 입력하세요.'}, status=400)
+                return JsonResponse({'MESSAGE':'이름을 입력하세요.'}, status=400)
             
             hash_password=bcrypt.hashpw(
                 password.encode('utf-8'),
