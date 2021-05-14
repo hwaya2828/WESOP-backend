6e380b49f7ca3581ea883f162acf1ff9ccbc685e
Mac <mac@Macui-MacBook-Pro.local>
2021-05-14 15:47:35 +0900

    [1차 프로젝트] wesop 로그인

--- users/views.py
+++ users/views.py
@@ -17,9 +17,9 @@ class SignupView(View):
 
             email        = data['email']
             password     = data['password']
-            first_name   = data['first_name']
-            last_name    = data['last_name']
-            phone_number = data.get('phone', None)
+            first_name   = data['firstname']
+            last_name    = data['lastname']
+            phone_number = data.get('phonenumber', None)
 
             if (re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.com$', data['email']) is None):
                 return JsonResponse({'MESSAGE':'유효하지 않은 Email'}, status=400)
@@ -59,3 +59,30 @@ class SignupView(View):
 
         except KeyError: 
             return JsonResponse({'MESSAGE':'KeyError'}, status=400)
+
+class LoginView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+
+        signin_email    = data['email']
+        signin_password = data['password']
+
+        if not signin_email and not signin_password:
+        	return JsonResponse({"MESSAGE":"KEYERROR"}, status=400)
+       
+        if not User.objects.filter(email=signin_email).exists():
+        	return JsonResponse({"MESSAGE":"INVALID_EMAIL"}, status=400)
+
+        user            = User.objects.get(email=signin_email)         
+        hashed_password = user.password.encode('utf-8')
+
+        if not bcrypt.checkpw(signin_password.encode('utf-8'), hashed_password):
+            return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
+        access_token = jwt.encode(
+                    {'user_id' : user.id, 'exp':datetime.utcnow()+timedelta(minutes=120)}, 
+                    SECRET, 
+                    algorithm = 'HS256'
+                )
+
+        return JsonResponse({"token":access_token} ,status=200)
