a2318523af52c3baa9c7fe7d4d69907a9b2f0ddb
hwaya2828 <hwaya2828@gmail.com>
2021-05-14 16:08:29 +0900

    ADD: LOAD OPEN DATA - 이솝 사이트 오픈 시 메뉴 & 카테고리 데이터 전달 역할

--- wesop/urls.py
+++ wesop/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls    import path, include
 
 urlpatterns = [
-    path('user', include('users.urls'))
+    path('user', include('users.urls')),
+    path('products', include('products.urls'))
 ]
