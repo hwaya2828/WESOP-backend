57b9036ce93dd95fc2d0e24184d1d4df0a8d3df6
hwaya2828 <hwaya2828@gmail.com>
2021-05-18 16:45:13 +0900

    REFACTOR: 코드 개선

--- products/views.py
+++ products/views.py
@@ -10,14 +10,20 @@ class MetaView(View):
     def get(self, request):
         menus = Menu.objects.all()
 
-        results = [{
-                    "menu_name"     : menu.name,
-                    "category_list" : [{
-                                        "category_name"     : category.name,
-                                        "description_title" : category.description_title if category.description_title else None,
-                                        "description"       : category.description if category.description else None
-                                        } for category in menu.category_set.all()]
-                    } for menu in menus]
+        results = [
+            {
+                "menu_name"     : menu.name,
+                "menu_id"       : menu.id,
+                "category_list" : [
+                    {
+                        "category_name"              : category.name,
+                        "category_id"                : category.id,
+                        "category_description_title" : category.description_title if category.description_title else None,
+                        "category_description"       : category.description if category.description else None
+                    } for category in menu.category_set.all()
+                ]
+            } for menu in menus
+        ]
 
         return JsonResponse({'result': results}, status=200)
 
@@ -46,8 +52,8 @@ class ProductListView(View):
 
             feature_result = [
                 {
-                        "feature_category_name" : feature_category.name,
-                        "features" : [feature.name for feature in feature_category.feature_set.filter(product=product)]
+                    "feature_category_name" : feature_category.name,
+                    "features"              : [feature.name for feature in feature_category.feature_set.filter(product=product)]
                 } for feature_category in set(FeatureCategory.objects.filter(feature__in=product.feature.all()))
             ]
 
@@ -104,8 +110,8 @@ class DetailProductView(View):
                         "product_description"       : product.description,
                         "product_content"           : product.content,
                         "product_content_image_url" : product.content_image_url,
-                        "product_ingredient_result" : [ingredient.name for ingredient in ingredients],
-                        "product_selection_result"  : [
+                        "product_ingredients"       : [ingredient.name for ingredient in ingredients],
+                        "product_selections"  : [
                             {
                                 "size"      : product_selection.size,
                                 "price"     : product_selection.price,
@@ -120,6 +126,7 @@ class DetailProductView(View):
                 "features"              : [feature.name for feature in feature_category.feature_set.filter(product=product)]
             } for feature_category in set(FeatureCategory.objects.filter(feature__in=product.feature.all()))
         ]
-        results["features"] = feature_result
+
+        results["product_features"] = feature_result
 
         return JsonResponse({'result':results}, status=200)
