ec94949b706cf09934f05321d1694e6fe0dbc34d
hwaya2828 <hwaya2828@gmail.com>
2021-05-18 16:12:05 +0900

    FIX: KEY값 통일

--- products/views.py
+++ products/views.py
@@ -15,10 +15,10 @@ class MetaView(View):
                 "menu_id"       : menu.id,
                 "category_list" : [
                     {
-                        "category_name"     : category.name,
-                        "category_id"       : category.id,
-                        "description_title" : category.description_title if category.description_title else None,
-                        "description"       : category.description if category.description else None
+                        "category_name"              : category.name,
+                        "category_id"                : category.id,
+                        "category_description_title" : category.description_title if category.description_title else None,
+                        "category_description"       : category.description if category.description else None
                     } for category in menu.category_set.all()
                 ]
             } for menu in menus
@@ -41,17 +41,17 @@ class DetailProductView(View):
         # product.save() 프로젝트 완성되면 카운트 쌓을 예정입니다 :)
 
         results = {
-                        "menu_name"                : menu.name,
-                        "menu_id"                  : menu.id,
-                        "category_name"            : category.name,
-                        "category_id"              : category.id,
-                        "product_name"             : product.name,
-                        "product_id"               : product.id,
-                        "description"              : product.description,
-                        "content"                  : product.content,
-                        "content_image_url"        : product.content_image_url,
-                        "ingredient_result"        : [ingredient.name for ingredient in ingredients],
-                        "product_selection_result" : [
+                        "menu_name"                 : menu.name,
+                        "menu_id"                   : menu.id,
+                        "category_name"             : category.name,
+                        "category_id"               : category.id,
+                        "product_name"              : product.name,
+                        "product_id"                : product.id,
+                        "product_description"       : product.description,
+                        "product_content"           : product.content,
+                        "product_content_image_url" : product.content_image_url,
+                        "product_ingredients"       : [ingredient.name for ingredient in ingredients],
+                        "product_selections"  : [
                             {
                                 "size"      : product_selection.size,
                                 "price"     : product_selection.price,
@@ -62,10 +62,10 @@ class DetailProductView(View):
 
         feature_result = [
             {
-                    "feature_category_name" : feature_category.name,
-                    "features" : [feature.name for feature in feature_category.feature_set.filter(product=product)]
+                "feature_category_name" : feature_category.name,
+                "features"              : [feature.name for feature in feature_category.feature_set.filter(product=product)]
             } for feature_category in set(FeatureCategory.objects.filter(feature__in=product.feature.all()))
         ]
-        results["feature"] = feature_result
+        results["product_features"] = feature_result
 
         return JsonResponse({'result':results}, status=200)
