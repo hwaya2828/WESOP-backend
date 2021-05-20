69fd63df168b9398b18525e54da2f40100b57553
Mac <mac@Macui-MacBook-Pro.local>
2021-05-20 12:28:23 +0900

    commit

--- orders/views.py
+++ orders/views.py
@@ -113,3 +113,52 @@ class CartView(View):
             return JsonResponse({'MESSAGE':'KEY ERROR'}, status=400)
         except Order.DoesNotExist:
             return JsonResponse({'MESSAGE':'nothing in cart'}, status=400)
+
+
+class OrderCheckView(View):
+    @Authorization_decorator
+    def get(self, request):
+        try:
+            user           = request.user
+            status_id      = OrderStatus.objects.get(name='주문 전').id
+            status_id_done = OrderStatus.objects.get(name='주문 후').id
+            
+            if (not Order.objects.filter(status_id=status_id, user_id=user.id)):
+                raise Exception
+
+            order          = Order.objects.get(status_id=status_id, user_id=user.id) 
+            cartlists      = OrderList.objects.filter(order_id=order.id)
+
+            result=[]
+
+            for cartlist in cartlists:
+                selection_id = cartlist.product_selection_id
+                select       = ProductSelection.objects.get(id=selection_id)
+                total        = select.price * cartlist.quantity
+
+                Order.objects.filter(status_id=status_id, user_id=user.id).update(
+                        status_id    = status_id_done, 
+                        address      = user.address,
+                        memo         = '',
+                        total_price  = total if (total >= 50000) else (total+3000), 
+                        free_delivery= True if (total >= 50000) else False 
+                    )
+            
+                order_dict = {
+                    'name'        : Product.objects.get(id=select.product_id).name,
+                    'quantity'    : cartlist.quantity ,
+                    'total_price' : Order.objects.get(id=cartlist.order_id).total_price,
+                    'purchased_at': Order.objects.get(id=cartlist.order_id).purchased_at,
+                    'address'     : User.objects.get(id=user.id).address
+                } 
+                result.append(order_dict)
+
+            OrderList.objects.filter(order_id=order.id).delete()
+
+            return JsonResponse({'result':result}, status=200)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY ERROR'}, status=400)
+
+        except Exception as e:
+            return JsonResponse({'MESSAGE':'nothing in cart'}, status=400)
