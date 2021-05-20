76ad764780325a46678161e3308323f3560f952e
Mac <mac@Macui-MacBook-Pro.local>
2021-05-20 12:18:36 +0900

    commit

--- users/views.py
+++ users/views.py
@@ -17,24 +17,24 @@ class SignupView(View):
 
             email        = data['email']
             password     = data['password']
-            first_name   = data['first_name']
-            last_name    = data['last_name']
-            phone_number = data.get('phone', None)
+            first_name   = data['firstname']
+            last_name    = data['lastname']
+            phone_number = data.get('phonenumber', None)
 
             if (re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.com$', data['email']) is None):
-                return JsonResponse({'MESSAGE':'유효하지 않은 Email'}, status=400)
+                return JsonResponse({'MESSAGE':'INVALID_EMAIL'}, status=400)
 
             if (User.objects.filter(email=email).exists()):
-                return JsonResponse({'MESSAGE':'이미 존재하는 계정입니다.'}, status=400)
+                return JsonResponse({'MESSAGE':'EXISTING_USER'}, status=400)
 
-            if (re.match('^[^A-Z]{6,10}$', data['password'])) or (re.match('^[^0-9]{6,10}$', data['password'])):
-                return JsonResponse({'MESSAGE':'대문자,숫자,6글자 이상,10글자 이하'}, status=400)
+            if not (re.match('^(?=.*[A-Z])(?=.*[0-9])[^ㄱ-ㅎ가-힣ㅏ-ㅣ]{6,10}$', data['password'])):
+                return JsonResponse({'MESSAGE':'INVALID_PASSWORD'}, status=400)
 
-            if not first_name: 
-                return JsonResponse({'MESSAGE':'성을 입력하세요.'}, status=400)
+            if not (re.match('^[가-힣a-zA-Z]{1,15}$', data['firstname'])):
+                return JsonResponse({'MESSAGE':'이름은 1글자 이상,15글자 이하,숫자가 아닌 입력.'}, status=400)
 
-            if not last_name:
-                return JsonResponse({'MESSAGE':'이름을 입력하세요.'}, status=400)
+            if not (re.match('^[가-힣a-zA-Z]{1,10}$', data['lastname'])):
+                return JsonResponse({'MESSAGE':'성은 1글자 이상,10글자 이하,숫자가 아닌 입력.'}, status=400)
             
             hash_password=bcrypt.hashpw(
                 password.encode('utf-8'),
@@ -49,13 +49,7 @@ class SignupView(View):
                 phone       =phone_number
                 )
 
-            access_token = jwt.encode(
-                    {'user_id' : user.id, 'exp':datetime.utcnow()+timedelta(minutes=120)}, 
-                    SECRET, 
-                    algorithm = 'HS256'
-                )
-
-            return JsonResponse({'token':access_token}, status=201)
+            return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
 
         except KeyError: 
             return JsonResponse({'MESSAGE':'KeyError'}, status=400)
@@ -69,20 +63,21 @@ class LoginView(View):
         signin_password = data['password']
 
         if not signin_email and not signin_password:
-        	return JsonResponse({"MESSAGE":"KEYERROR"}, status=400)
-       
+            return JsonResponse({"MESSAGE":"KEYERROR"}, status=400)
+
         if not User.objects.filter(email=signin_email).exists():
-        	return JsonResponse({"MESSAGE":"INVALID_EMAIL"}, status=400)
+            return JsonResponse({"MESSAGE":"INVALID_EMAIL"}, status=400)
 
         user            = User.objects.get(email=signin_email)         
         hashed_password = user.password.encode('utf-8')
 
         if not bcrypt.checkpw(signin_password.encode('utf-8'), hashed_password):
-            return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+            return JsonResponse({'MESSAGE':'INVALID_PASSWORD'}, status=401)
+
         access_token = jwt.encode(
                     {'user_id' : user.id, 'exp':datetime.utcnow()+timedelta(minutes=120)}, 
                     SECRET, 
                     algorithm = 'HS256'
                 )
 
-        return JsonResponse({"token":access_token} ,status=200)
+        return JsonResponse({"token":access_token, "MESSAGE":"SUCCESS"} ,status=200)
