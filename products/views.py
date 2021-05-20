26988ec25c2691f44a1fc0a9eb8bedbbe58ef0fb
hwaya2828 <hwaya2828@gmail.com>
2021-05-20 11:30:37 +0900

    REFACTOR: 코드 개선

--- products/views.py
+++ products/views.py
@@ -133,3 +133,50 @@ class DetailProductView(View):
         results["product_features"] = feature_result
 
         return JsonResponse({'result': results}, status=200)
+
+class PopularProduct(View):
+    def get(self, request):
+        COUTN_RANKING = 5
+        products = Product.objects.all().order_by('-count')[:COUTN_RANKING]
+
+        total_results = []
+
+        for product in products:
+            category = product.category
+            menu     = category.menu
+
+            feature_result = [
+                {
+                    "feature_category_name" : feature_category.name,
+                    "features"              : [feature.name for feature in feature_category.feature_set.filter(product=product)]
+                } for feature_category in set(FeatureCategory.objects.filter(feature__in=product.feature.all()))
+            ]
+
+            results = [
+                {
+                    "menu_name"                  : menu.name,
+                    "menu_id"                    : menu.id,
+                    "category_name"              : category.name,
+                    "category_id"                : category.id,
+                    "category_description_title" : category.description_title,
+                    "category_description"       : category.description,
+                    "product_name"               : product.name,
+                    "product_id"                 : product.id,
+                    "product_description"        : product.description,
+                    "product_features"           : feature_result,
+                    "product_content"            : product.content,
+                    "product_content_image_url"  : product.content_image_url,
+                    "product_ingredients"        : [ingredient.name for ingredient in product.ingredient.all()],
+                    "product_selections"         : [
+                        {
+                            "size"      : product_selection.size,
+                            "price"     : product_selection.price,
+                            "image_url" : product_selection.image_url
+                        } for product_selection in product.productselection_set.all()
+                    ]
+                }
+            ]
+
+            total_results.append(results)
+
+        return JsonResponse({'result': total_results}, status=200)
